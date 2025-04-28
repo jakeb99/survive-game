29ed6d314e0055f0f5a99a499a41a6c31cb9d29a
Jake B <46630765+jakeb99@users.noreply.github.com>
2025-04-28 13:44:03 -0700

    add turret

--- Assets/Scripts/Weapons/TurretController.cs
+++ Assets/Scripts/Weapons/TurretController.cs
@@ -1,13 +1,24 @@
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
 
 public class TurretController : MonoBehaviour
 {
+    [Header("Turret Settings")]
     [SerializeField] Transform turretBasePivot;
     [SerializeField] Transform turretGunPivot;
-    [SerializeField] Transform weaponTip;
+    [SerializeField] string enemyTag = "Enemy";
+    [SerializeField] float turretRange;
+    [SerializeField] float rotationSpeed;
 
     [SerializeField] GameObject currentTarget;
-    [SerializeField] float colliderRadius;
+
+    [Header("Shooting Settings")]
+    //[SerializeField] private float fireRate;
+    //[SerializeField] private float fireCooldown = 0f;
+    [SerializeField] Transform weaponTip;
+
 
     private AttackAbility attackAbility;
 
@@ -18,19 +29,88 @@ public class TurretController : MonoBehaviour
 
     private void Start()
     {
-        CapsuleCollider capCollider = GetComponent<CapsuleCollider>();
-        capCollider.height = 10f;
-        capCollider.radius = colliderRadius;
-        capCollider.center = new Vector3(-colliderRadius, 0, 0);
+        InvokeRepeating("UpdateCurrentTarget", 0, 0.5f);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        if (other.gameObject.tag == "Enemy")
+        if (currentTarget == null) 
         {
-            Debug.Log("turret: enemy trigger entered");
+            attackAbility.StopAttack();
+            return;
+        }
+
+        FaceEnemy();
+        //shootingController.Shoot(weaponTip);
+
+        //if (fireCooldown <= 0f)
+        //{
+        //    Shoot();
+        //    fireCooldown = 1f/fireRate;
+        //}
+
+        //fireCooldown -= Time.deltaTime;
+        attackAbility.StartAttack(currentTarget);
+    }
+
+    void UpdateCurrentTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        float closestEnemyDist = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(enemy.transform.position, transform.position);
+            // if the enemy is in range
+            if (distanceToEnemy < closestEnemyDist)
+            {
+                closestEnemyDist = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if ( nearestEnemy != null && closestEnemyDist <= turretRange)
+        {
+            currentTarget = nearestEnemy;
+        } else
+        {
+            currentTarget = null;
         }
     }
 
+    private void FaceEnemy()
+    {
+        Quaternion rotateBy90Deg = Quaternion.AngleAxis(90, Vector3.up);
+
+        // get hdirection to face
+        Vector3 baseDirVec = currentTarget.transform.position - turretBasePivot.position;
+        
+        baseDirVec.Normalize();
+        Vector3 baseRot = baseDirVec; // only rotate on y axis
+        baseRot.y =  0f; // only rotate on y axis
+        Quaternion baseRotation = Quaternion.LookRotation(baseRot) * rotateBy90Deg;
+        turretBasePivot.rotation = Quaternion.Slerp(turretBasePivot.rotation, baseRotation, rotationSpeed * Time.deltaTime);
+
+        // get pitch
+        Vector3 gunDir = (currentTarget.transform.position) - turretGunPivot.position;
+        gunDir.y += currentTarget.transform.localScale.y / 2;       // add half its hight so we are looking at the middle of the enemy not at its feet
+        gunDir.Normalize();
+        Quaternion gunRotation = Quaternion.LookRotation(gunDir) * rotateBy90Deg;
+        turretGunPivot.rotation = Quaternion.Slerp(turretGunPivot.rotation, gunRotation, rotationSpeed * Time.deltaTime);
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        if (currentTarget)
+        {
+            Gizmos.DrawLine(turretBasePivot.position, currentTarget.transform.position);
+        }
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, turretRange);
+        Gizmos.DrawSphere(weaponTip.position, 0.05f);
+    }
 
 }
