472f94c0b82a448ba6cfd5b2493c4a91566d7846
Jake B <46630765+jakeb99@users.noreply.github.com>
2025-04-28 13:44:29 -0700

    rotate healthbar to face scene cam

--- Assets/Scripts/Health/HealthBar.cs
+++ Assets/Scripts/Health/HealthBar.cs
@@ -5,10 +5,37 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Image healthBarSprite;
     [SerializeField] private Health health;
+    [SerializeField] private Canvas healthCanvas;
+    [SerializeField] private float maxShowTimeAfterUpdate;
+    [SerializeField] private Transform sceneCam;
+    private float currentTime;
+    bool showCanvas;
+
+    private void Update()
+    {
+        RotateToFaceSceneCamera();
+        if (showCanvas && healthCanvas.enabled)
+        {
+            // start the timer
+            if (currentTime > maxShowTimeAfterUpdate)
+            {
+                currentTime = 0;
+                healthCanvas.enabled = false;
+                showCanvas = false;
+            }
+            else
+            {
+                currentTime += Time.deltaTime;
+            }
+        }
+    }
 
     private void Start()
     {
+        sceneCam = GameManager.Instance.SceneCamera.transform;
+
         health = GetComponentInParent<Health>();
+        healthCanvas.enabled = false;
 
         if (health == null)
         {
@@ -18,10 +45,29 @@ public class HealthBar : MonoBehaviour
             health.OnIncrementHealth += UpdateHealthBar;
             health.OnDecrementHealth += UpdateHealthBar;
         }
+
+        // rotate the healthbar to always face the camera
+        RotateToFaceSceneCamera();
     }
 
     public void UpdateHealthBar()
     {
+        if (!healthCanvas.enabled) 
+        { 
+            healthCanvas.enabled = true;
+            showCanvas = true;
+        }
+
         healthBarSprite.fillAmount = health.currentHealth / health.maxHealth;
+    }
+
+    private void RotateToFaceSceneCamera()
+    {
+        Vector3 dirVec = sceneCam.transform.position - gameObject.transform.position;
+
+        dirVec.Normalize();
+        //dirVec.y = 0;  // sp we only rotate along the y axis
+
+        gameObject.transform.rotation = Quaternion.LookRotation(dirVec);
     }
 }
