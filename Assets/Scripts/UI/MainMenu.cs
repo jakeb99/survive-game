891322d4dbc5199576f5a060fca622df05ab1c0a
Jake B <46630765+jakeb99@users.noreply.github.com>
2025-05-06 10:59:28 -0700

    add UI sounds to main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -6,6 +6,9 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private UIDocument doc;
     [SerializeField] private DataPersistenceManager saveSystem;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip buttonHoverClip;
+    [SerializeField] private AudioClip buttonClickedClip;
 
     private Button playButton;
     private Button exitButton;
@@ -17,6 +20,22 @@ public class MainMenu : MonoBehaviour
 
         exitButton = doc.rootVisualElement.Q<Button>("ExitBtn");
         exitButton.clicked += ExitButtonOnClicked;
+
+        Button[] buttons = {playButton, exitButton};
+
+        foreach (var button in buttons)
+        {
+            // onHover sound
+            button.RegisterCallback<PointerEnterEvent>(e =>
+            {
+                PlaySound(buttonHoverClip);
+            });
+
+            button.RegisterCallback<ClickEvent>(e =>
+            {
+                PlaySound(buttonClickedClip);
+            });
+        }
     }
 
     private void PlayButtonOnClicked()
@@ -28,5 +47,13 @@ public class MainMenu : MonoBehaviour
     private void ExitButtonOnClicked()
     {
         Application.Quit();
+    }
+
+    private void PlaySound(AudioClip clip)
+    {
+        if (clip != null)
+        {
+            audioSource.PlayOneShot(clip);
+        }
     }
 }
